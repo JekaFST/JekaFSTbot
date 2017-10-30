cd77be5288c45f498f5154557abf94a42750f07f
ereshetnikov <jekafst@gmail.com>
2017-10-30 21:42:13 +0300

    Parcers refactoring

--- UpdaterMethods.py
+++ UpdaterMethods.py
@@ -163,8 +163,7 @@ def sectors_parcer(loaded_sectors, codes_to_find, sector_statuses, bot, chat_id)
             if not sector['SectorId'] in sector_statuses.keys():
                 sector_statuses[sector['SectorId']] = {'answer_info_not_sent': True}
 
-            sector_info_not_sent = get_sector_status(sector, sector_statuses)
-            if sector['IsAnswered'] and sector_info_not_sent:
+            if sector['IsAnswered'] and sector_statuses[sector['SectorId']]['answer_info_not_sent']:
 
                 code = sector['Answer']['Answer'].encode('utf-8')
                 player = sector['Answer']['Login'].encode('utf-8')
@@ -175,16 +174,7 @@ def sectors_parcer(loaded_sectors, codes_to_find, sector_statuses, bot, chat_id)
                           '\r\nОсталось закрыть: %s из %s:\r\n%s' % (str(codes_to_find), str(codes_all),
                                                                      sectors_to_close)
                 bot.send_message(chat_id, message, parse_mode='HTML')
-                for k, v in sector_statuses.items():
-                    if sector['SectorId'] == k:
-                        v['answer_info_not_sent'] = False
-
-
-def get_sector_status(sector, sector_statuses):
-    for k, v in sector_statuses.items():
-        if sector['SectorId'] == k:
-            answer_info_not_sent = v['answer_info_not_sent']
-            return answer_info_not_sent
+                sector_statuses[sector['SectorId']]['answer_info_not_sent'] = False
 
 
 def help_parcer(loaded_helps, help_statuses, bot, chat_id, channel_name, use_channel):
@@ -193,30 +183,16 @@ def help_parcer(loaded_helps, help_statuses, bot, chat_id, channel_name, use_cha
             if not help['HelpId'] in help_statuses.keys():
                 help_statuses[help['HelpId']] = {'not_sent': True, 'time_not_sent': True}
 
-            not_sent, time_not_sent = get_help_status(help, help_statuses)
-
-            if not_sent and help['HelpText'] is not None:
+            if help_statuses[help['HelpId']]['not_sent'] and help['HelpText'] is not None:
                 send_help(help, bot, chat_id)
                 if channel_name and use_channel:
                     send_help(help, bot, channel_name)
-                for k, v in help_statuses.items():
-                    if help['HelpId'] == k:
-                        v['not_sent'] = False
-                        v['time_not_sent'] = False
+                help_statuses[help['HelpId']]['not_sent'] = False
+                help_statuses[help['HelpId']]['time_not_sent'] = False
                 continue
-            if time_not_sent and help['RemainSeconds'] <= 180:
+            if help_statuses[help['HelpId']]['time_not_sent'] and help['RemainSeconds'] <= 180:
                 send_time_to_help(help, bot, chat_id)
-                for k, v in help_statuses.items():
-                    if help['HelpId'] == k:
-                        v['time_not_sent'] = False
-
-
-def get_help_status(help, help_statuses):
-    for k, v in help_statuses.items():
-        if help['HelpId'] == k:
-            not_sent = v['not_sent']
-            time_not_sent = v['time_not_sent']
-            return not_sent, time_not_sent
+                help_statuses[help['HelpId']]['time_not_sent'] = False
 
 
 def bonus_parcer(loaded_bonuses, bonus_statuses, game_answered_bonus_ids, bot, chat_id):
@@ -225,29 +201,15 @@ def bonus_parcer(loaded_bonuses, bonus_statuses, game_answered_bonus_ids, bot, c
             if not bonus['BonusId'] in bonus_statuses.keys():
                 bonus_statuses[bonus['BonusId']] = {'info_not_sent': True, 'award_not_sent': True}
 
-            info_not_sent, award_not_sent = get_bonus_status(bonus, bonus_statuses)
-
-            if bonus['IsAnswered'] and award_not_sent:
+            if bonus['IsAnswered'] and bonus_statuses[bonus['BonusId']]['award_not_sent']:
                 send_bonus_award_answer(bonus, bot, chat_id)
                 game_answered_bonus_ids.append(bonus['BonusId'])
-                for k, v in bonus_statuses.items():
-                    if bonus['BonusId'] == k:
-                        v['award_not_sent'] = False
-                        v['info_not_sent'] = False
+                bonus_statuses[bonus['BonusId']]['award_not_sent'] = False
+                bonus_statuses[bonus['BonusId']]['info_not_sent'] = False
                 continue
-            if info_not_sent and bonus['Task'] and not bonus['Expired']:
+            if bonus_statuses[bonus['BonusId']]['info_not_sent'] and bonus['Task'] and not bonus['Expired']:
                 send_bonus_info(bonus, bot, chat_id)
-                for k, v in bonus_statuses.items():
-                    if bonus['BonusId'] == k:
-                        v['info_not_sent'] = False
-
-
-def get_bonus_status(bonus, bonus_statuses):
-    for k, v in bonus_statuses.items():
-        if bonus['BonusId'] == k:
-            info_not_sent = v['info_not_sent']
-            award_not_sent = v['award_not_sent']
-            return info_not_sent, award_not_sent
+                bonus_statuses[bonus['BonusId']]['info_not_sent'] = False
 
 
 def message_parcer(loaded_messages, message_statuses, sent_messages, bot, chat_id, channel_name, use_channel):
@@ -274,8 +236,6 @@ def channel_sectors_editor(loaded_level, old_sectors_to_close, bot, channel_name
         bot.edit_message_text(message, channel_name, message_id, parse_mode='HTML')
 
     return new_sectors_to_close
-    # else:
-    #     return old_sectors_to_close
 
 
 def get_sectors_to_close(sectors, get_sector_names=False):
