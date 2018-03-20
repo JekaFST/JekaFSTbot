b402a2274723be3200dbc4b02d373eb14ed64709
JekaFST <jekafst@gmail.com>
2018-03-20 22:03:05 +0300

    Update BotService.py

--- BotService.py
+++ BotService.py
@@ -728,7 +728,7 @@ def run_app(bot, main_vars):
     bot.remove_webhook()
 
     # Set webhook
-    bot.set_webhook(url='https://ec374396.ngrok.io/webhook')
+    bot.set_webhook(url='https://powerful-shelf-32284.herokuapp.com/webhook')
 
     @app.route("/", methods=['GET', 'POST'])
     def hello():
