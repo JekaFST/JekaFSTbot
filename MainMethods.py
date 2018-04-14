e350fe4c3188f820c131503e4707db34cf13bb51
ereshetnikov <jekafst@gmail.com>
2018-04-14 21:10:28 +0300

    add custom ll fix

--- MainMethods.py
+++ MainMethods.py
@@ -480,5 +480,5 @@ def add_custom_live_locations(task, bot):
     ll_message_ids = json.loads(session['llmessageids'])
     if ll_message_ids:
         close_live_locations(task.chat_id, bot, task.session, ll_message_ids)
-    send_live_locations_to_chat(bot, task.chat_id, task.session, locations, ll_message_ids,
+    send_live_locations_to_chat(bot, task.chat_id, session, locations, ll_message_ids,
                                 custom_points=task.points_dict, duration=task.duration)
