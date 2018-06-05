be811209c24dc54f7e57ca49e47d196a4816cdb0
Evgeniy Reshetnikov <ereshetnikov@therecongroup.com>
2018-06-05 13:45:49 +0300

    Minor refactoring for game builder

--- GameDetailsBuilder.py
+++ GameDetailsBuilder.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from Methods import GoogleDocConnection, ENConnection, make_help_data_and_url, make_bonus_data_and_url, \
+from GameDetailsBuilderMethods import GoogleDocConnection, ENConnection, make_help_data_and_url, make_bonus_data_and_url, \
     make_sector_data_and_url, make_penalty_help_data_and_url
 from time import sleep
 
@@ -8,11 +8,11 @@ login, password, domain, gameid = google_doc_connection.get_setup()
 en_connection = ENConnection(domain, login, password, gameid)
 
 
-# for i, help in enumerate(google_doc_connection.get_helps()):
-#     if i % 30 == 0:
-#         sleep(5)
-#     help_data, help_url = make_help_data_and_url(help, domain, gameid)
-#     en_connection.create_en_object(help_url, help_data, 'help')
+for i, help in enumerate(google_doc_connection.get_helps()):
+    if i % 30 == 0:
+        sleep(5)
+    help_data, help_url = make_help_data_and_url(help, domain, gameid)
+    en_connection.create_en_object(help_url, help_data, 'help')
 
 for i, bonus in enumerate(google_doc_connection.get_bonuses()):
     if i % 30 == 0:
@@ -20,14 +20,14 @@ for i, bonus in enumerate(google_doc_connection.get_bonuses()):
     bonus_data, bonus_url = make_bonus_data_and_url(bonus, domain, gameid, en_connection.level_ids_dict)
     en_connection.create_en_object(bonus_url, bonus_data, 'bonus')
 
-# for i, sector in enumerate(google_doc_connection.get_sectors()):
-#     if i % 30 == 0:
-#         sleep(5)
-#     sector_data, sector_url = make_sector_data_and_url(sector, domain, gameid)
-#     en_connection.create_en_object(sector_url, sector_data, 'sector')
-#
-# for i, penalty_help in enumerate(google_doc_connection.get_penalty_helps()):
-#     if i % 30 == 0:
-#         sleep(5)
-#     pen_help_data, pen_help_url = make_penalty_help_data_and_url(penalty_help, domain, gameid)
-#     en_connection.create_en_object(pen_help_url, pen_help_data, 'PenaltyHelp')
+for i, sector in enumerate(google_doc_connection.get_sectors()):
+    if i % 30 == 0:
+        sleep(5)
+    sector_data, sector_url = make_sector_data_and_url(sector, domain, gameid)
+    en_connection.create_en_object(sector_url, sector_data, 'sector')
+
+for i, penalty_help in enumerate(google_doc_connection.get_penalty_helps()):
+    if i % 30 == 0:
+        sleep(5)
+    pen_help_data, pen_help_url = make_penalty_help_data_and_url(penalty_help, domain, gameid)
+    en_connection.create_en_object(pen_help_url, pen_help_data, 'PenaltyHelp')
--- GameDetailsBuilderMethods.py
+++ GameDetailsBuilderMethods.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
