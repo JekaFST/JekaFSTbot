f5e4b9126ad89f3d8862b804d0d2a4e92c2cc2ac
ereshetnikov <jekafst@gmail.com>
2018-08-19 12:59:51 +0300

    fix logging

--- BotServiceMethods.py
+++ BotServiceMethods.py
@@ -46,37 +46,37 @@ def add_level_bonuses(levels_dict, bonus_lines):
 
 
 def run_db_cleanup(bot):
-    logging.info("DB cleanup is launched")
+    logging.log("DB cleanup is launched")
     try:
         urls_messages = DB.get_gameurls_messages()
-        logging.info("urls_messages got")
+        logging.log("urls_messages got")
         elements_cleanup(urls_messages, 'messages')
-        logging.info("After messages cleanup started")
+        logging.log("After messages cleanup started")
 
         urls_levels = DB.get_gameurls_levels()
-        logging.info("urls_levels got")
+        logging.log("urls_levels got")
         elements_cleanup(urls_levels, 'levels')
-        logging.info("After levels cleanup started")
+        logging.log("After levels cleanup started")
 
         urls_helps = DB.get_gameurls_helps()
-        logging.info("urls_helps got")
+        logging.log("urls_helps got")
         elements_cleanup(urls_helps, 'helps')
-        logging.info("After helps cleanup started")
+        logging.log("After helps cleanup started")
 
         urls_bonuses = DB.get_gameurls_bonuses()
-        logging.info("urls_bonuses got")
+        logging.log("urls_bonuses got")
         elements_cleanup(urls_bonuses, 'bonuses')
-        logging.info("After bonuss cleanup started")
+        logging.log("After bonuss cleanup started")
 
         urls_sectors = DB.get_gameurls_sectors()
-        logging.info("urls_sectors got")
+        logging.log("urls_sectors got")
         elements_cleanup(urls_sectors, 'sectors')
-        logging.info("After sectors cleanup started")
+        logging.log("After sectors cleanup started")
 
         bot.send_message(45839899, 'db_cleanup выполнен')
     except Exception:
         logging.exception("Exception during DB cleanup")
-    logging.info("DB cleanup is finished")
+    logging.log("DB cleanup is finished")
     return
 
 
