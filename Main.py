f5d64fc7cfe9f7f340e1bb83943de20c3feae32a
ereshetnikov <jekafst@gmail.com>
2018-09-20 22:56:35 +0300

    Counter added

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import threading
+import time
 from Queue import PriorityQueue
 import telebot
 from Const import prod
@@ -10,8 +11,23 @@ from MainMethods import reload_backup
 from TaskMathodMap import TaskMethodMap
 
 
+class Counter(object):
+    def __init__(self):
+        self.code_tasks_in = 0
+        self.code_tasks_out = 0
+        self.a = float()
+        self.b = float()
+
+    def drop_counter(self):
+        self.code_tasks_in = 0
+        self.code_tasks_out = 0
+        self.a = float()
+        self.b = float()
+
+
 port = int(os.environ.get('PORT', 5000)) if prod else 443
 
+counter = Counter()
 queue = PriorityQueue()
 bot = telebot.TeleBot(DB.get_main_bot_token()) if prod else telebot.TeleBot("583637976:AAEFrQFiAaGuKwmoRV0N1MwU-ujRzmCxCAo")
 bot.remove_webhook()
@@ -26,6 +42,22 @@ except Exception:
 reload_backup(bot, queue)
 
 while True:
+    if counter.a and counter.b:
+        print str(counter.b - counter.a)
+        counter.drop_counter()
     if not queue.empty():
-        TaskMethodMap.run_task(queue.get()[1], bot)
+        task = queue.get()[1]
+        if task.type == 'send_code_bonus':
+            counter.code_tasks_in += 1
+        if counter.code_tasks_in == 1:
+            counter.a = time.time()
+        TaskMethodMap.run_task(task, bot)
         queue.task_done()
+        if task.type == 'send_code_bonus':
+            counter.code_tasks_out += 1
+        if counter.code_tasks_out == 100:
+            counter.b = time.time()
+
+    # if not queue.empty():
+    #     TaskMethodMap.run_task(queue.get()[1], bot)
+    #     queue.task_done()
