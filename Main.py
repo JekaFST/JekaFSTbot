77c648bcbe8b7c8a6beb06e11fe5f3a75a8e63a0
Evgeniy Reshetnikov <ereshetnikov@therecongroup.com>
2018-09-21 11:01:40 +0300

    del counter

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 import threading
-import time
 from Queue import PriorityQueue
 import telebot
 from Const import prod, num_worker_threads
@@ -11,46 +10,16 @@ from MainMethods import reload_backup
 from TaskMathodMap import TaskMethodMap
 
 
-class Counter(object):
-    def __init__(self):
-        self.code_tasks_in = 0
-        self.code_tasks_out = 0
-        self.a = float()
-        self.b = float()
-
-    def drop_counter(self):
-        self.code_tasks_in = 0
-        self.code_tasks_out = 0
-        self.a = float()
-        self.b = float()
-
-
 def worker():
     while True:
-        if counter.a and counter.b:
-            print str(counter.b - counter.a)
-            counter.drop_counter()
         if not queue.empty():
             TaskMethodMap.run_task(queue.get()[1], bot)
-            task = queue.get()[1]
-            if task.type == 'send_code_bonus':
-                counter.code_tasks_in += 1
-            if counter.code_tasks_in == 1:
-                counter.a = time.time()
-            TaskMethodMap.run_task(task, bot)
             queue.task_done()
-            if task.type == 'send_code_bonus':
-                counter.code_tasks_out += 1
-            if counter.code_tasks_out == 100:
-                counter.b = time.time()
-        # if not queue.empty():
-        #     TaskMethodMap.run_task(queue.get()[1], bot)
-        #     queue.task_done()
 
 
 port = int(os.environ.get('PORT', 5000)) if prod else 443
 queue = PriorityQueue()
-counter = Counter()
+
 bot = telebot.TeleBot(DB.get_main_bot_token()) if prod else telebot.TeleBot("583637976:AAEFrQFiAaGuKwmoRV0N1MwU-ujRzmCxCAo")
 bot.remove_webhook()
 bot.set_webhook(url='https://powerful-shelf-32284.herokuapp.com/webhook') if prod \
