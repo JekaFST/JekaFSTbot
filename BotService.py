047e79ad23e3cb239bd1043b664e2dbe30f9d36c
eresh_intelocate <evgeniy@intelocate.com>
2023-01-09 12:14:36 +0300

    fixie

--- BotService.py
+++ BotService.py
@@ -186,7 +186,7 @@ def run_app():
                 and Validations.check_session_available(message.chat.id, bot) \
                 and Validations.check_join_possible(message.chat.id, bot, message.from_user.id, message.message_id, add_chat_ids):
             join_task = Task(message.chat.id, 'join', message_id=message.message_id, user_id=message.from_user.id)
-            queue.put((99, join_task))
+            queue.queue.put((99, join_task))
 
     @bot.message_handler(commands=['reset_join'])
     def reset_join(message):
@@ -195,7 +195,7 @@ def run_app():
                 and Validations.check_session_available(message.chat.id, bot) \
                 and Validations.check_reset_join_possible(message.chat.id, bot, message.from_user.id, message.message_id, add_chat_ids):
             reset_join_task = Task(message.chat.id, 'reset_join', message_id=message.message_id, user_id=message.from_user.id)
-            queue.put((99, reset_join_task))
+            queue.queue.put((99, reset_join_task))
 
     @bot.message_handler(commands=['add'])
     def add_chat_to_allowed(message):
@@ -245,7 +245,7 @@ def run_app():
         allowed, main_chat_ids, add_chat_ids = Validations.check_permission(message.chat.id, bot)
         if allowed and not Validations.check_from_add_chat(message.chat.id, add_chat_ids):
             start_task = Task(message.chat.id, 'start')
-            queue.put((99, start_task))
+            queue.queue.put((99, start_task))
 
     @bot.message_handler(commands=['help'])
     def help(message):
@@ -260,7 +260,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             start_session_task = Task(message.chat.id, 'start_session', session_id=message.chat.id)
-            queue.put((99, start_session_task))
+            queue.queue.put((99, start_session_task))
 
     @bot.message_handler(commands=['stop_session'])
     def stop_session(message):
@@ -269,7 +269,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             stop_session_task = Task(message.chat.id, 'stop_session', session_id=message.chat.id)
-            queue.put((99, stop_session_task))
+            queue.queue.put((99, stop_session_task))
 
     @bot.message_handler(commands=['config'])
     def config(message):
@@ -278,7 +278,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             config_task = Task(message.chat.id, 'config', session_id=message.chat.id)
-            queue.put((99, config_task))
+            queue.queue.put((99, config_task))
 
     @bot.message_handler(commands=['login'])
     def save_login(message):
@@ -291,7 +291,7 @@ def run_app():
                 bot.send_message(message.chat.id, 'Введите логин после команды /login, через пробел', reply_to_message_id=message.message_id)
                 return
             set_login_task = Task(message.chat.id, 'login', session_id=main_chat_id, new_login=new_login)
-            queue.put((99, set_login_task))
+            queue.queue.put((99, set_login_task))
 
     @bot.message_handler(commands=['password'])
     def save_password(message):
@@ -304,7 +304,7 @@ def run_app():
                 bot.send_message(message.chat.id, 'Введите пароль после команды /password, через пробел', reply_to_message_id=message.message_id)
                 return
             set_password_task = Task(message.chat.id, 'password', session_id=main_chat_id, new_password=new_password)
-            queue.put((99, set_password_task))
+            queue.queue.put((99, set_password_task))
 
     @bot.message_handler(commands=['domain'])
     def save_en_domain(message):
@@ -317,7 +317,7 @@ def run_app():
                 bot.send_message(message.chat.id, 'Введите домен после команды /domain, через пробел', reply_to_message_id=message.message_id)
                 return
             set_domain_task = Task(message.chat.id, 'domain', session_id=main_chat_id, new_domain=new_domain)
-            queue.put((99, set_domain_task))
+            queue.queue.put((99, set_domain_task))
 
     @bot.message_handler(commands=['gameid'])
     def save_game_id(message):
@@ -330,7 +330,7 @@ def run_app():
                 bot.send_message(message.chat.id, 'Введите айди игры после команды /gameid, через пробел', reply_to_message_id=message.message_id)
                 return
             set_game_id_task = Task(message.chat.id, 'game_id', session_id=main_chat_id, new_game_id=new_game_id)
-            queue.put((99, set_game_id_task))
+            queue.queue.put((99, set_game_id_task))
 
     @bot.message_handler(commands=['login_to_en'])
     def login_to_en(message):
@@ -339,7 +339,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             login_to_en_task = Task(message.chat.id, 'login_to_en', session_id=message.chat.id)
-            queue.put((99, login_to_en_task))
+            queue.queue.put((99, login_to_en_task))
 
     @bot.message_handler(commands=['task'])
     def send_task(message):
@@ -350,7 +350,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_task_task = Task(message.chat.id, 'send_task', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_task_task))
+            queue.queue.put((99, send_task_task))
 
     @bot.message_handler(commands=['task_images'])
     def send_task_images(message):
@@ -360,7 +360,7 @@ def run_app():
 
             main_chat_id = message.chat.id if message.chat.id in main_chat_ids else DB.get_main_chat_id_via_add(message.chat.id)
             send_task_images_task = Task(message.chat.id, 'task_images', session_id=main_chat_id)
-            queue.put((99, send_task_images_task))
+            queue.queue.put((99, send_task_images_task))
 
     @bot.message_handler(commands=['sectors'])
     def send_all_sectors(message):
@@ -371,7 +371,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_all_sectors_task = Task(message.chat.id, 'send_sectors', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_all_sectors_task))
+            queue.queue.put((99, send_all_sectors_task))
 
     @bot.message_handler(commands=['hints'])
     def send_all_helps(message):
@@ -382,7 +382,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_all_helps_task = Task(message.chat.id, 'send_helps', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_all_helps_task))
+            queue.queue.put((99, send_all_helps_task))
 
     @bot.message_handler(commands=['last_hint'])
     def send_last_help(message):
@@ -393,7 +393,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_last_help_task = Task(message.chat.id, 'send_last_help', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_last_help_task))
+            queue.queue.put((99, send_last_help_task))
 
     @bot.message_handler(commands=['bonuses'])
     def send_all_bonuses(message):
@@ -404,7 +404,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_all_bonuses_task = Task(message.chat.id, 'send_bonuses', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_all_bonuses_task))
+            queue.queue.put((99, send_all_bonuses_task))
 
     @bot.message_handler(commands=['unclosed_bonuses'])
     def send_unclosed_bonuses(message):
@@ -415,7 +415,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_unclosed_bonuses_task = Task(message.chat.id, 'unclosed_bonuses', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_unclosed_bonuses_task))
+            queue.queue.put((99, send_unclosed_bonuses_task))
 
     @bot.message_handler(commands=['messages'])
     def send_auth_messages(message):
@@ -426,7 +426,7 @@ def run_app():
             storm_level = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0)) if \
                 re.findall(r'[\d]+', str(message.text.encode('utf-8'))) else None
             send_auth_messages_task = Task(message.chat.id, 'send_messages', session_id=main_chat_id, storm_level_number=storm_level)
-            queue.put((99, send_auth_messages_task))
+            queue.queue.put((99, send_auth_messages_task))
 
     @bot.message_handler(commands=['start_updater'])
     def start_updater(message):
@@ -435,7 +435,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             start_updater_task = Task(message.chat.id, 'start_updater', queue=queue, session_id=message.chat.id)
-            queue.put((2, start_updater_task))
+            queue.queue.put((2, start_updater_task))
 
     @bot.message_handler(commands=['delay'])
     def set_updater_delay(message):
@@ -445,7 +445,7 @@ def run_app():
 
             new_delay = int(re.search(r'[\d]+', str(message.text.encode('utf-8'))).group(0))
             set_delay_task = Task(message.chat.id, 'delay', session_id=message.chat.id, new_delay=new_delay)
-            queue.put((99, set_delay_task))
+            queue.queue.put((99, set_delay_task))
 
     @bot.message_handler(commands=['stop_updater'])
     def stop_updater(message):
@@ -454,7 +454,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             stop_updater_task = Task(message.chat.id, 'stop_updater', session_id=message.chat.id)
-            queue.put((2, stop_updater_task))
+            queue.queue.put((2, stop_updater_task))
 
     @bot.message_handler(commands=['set_channel_name'])
     def set_channel_name(message):
@@ -467,7 +467,7 @@ def run_app():
                 bot.send_message(message.chat.id, 'Введите имя канала после команды /set_channel_name, через пробел', reply_to_message_id=message.message_id)
                 return
             set_channel_name_task = Task(message.chat.id, 'channel_name', session_id=main_chat_id, new_channel_name=new_channel_name)
-            queue.put((99, set_channel_name_task))
+            queue.queue.put((99, set_channel_name_task))
 
     @bot.message_handler(commands=['start_channel'])
     def start_channel(message):
@@ -476,7 +476,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             start_channel_task = Task(message.chat.id, 'start_channel', session_id=message.chat.id)
-            queue.put((99, start_channel_task))
+            queue.queue.put((99, start_channel_task))
 
     @bot.message_handler(commands=['stop_channel'])
     def stop_channel(message):
@@ -485,7 +485,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             stop_channel_task = Task(message.chat.id, 'stop_channel', session_id=message.chat.id)
-            queue.put((99, stop_channel_task))
+            queue.queue.put((99, stop_channel_task))
 
     @bot.message_handler(commands=['codes_on'])
     def enable_codes(message):
@@ -494,7 +494,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             enable_codes_task = Task(message.chat.id, 'codes_on', session_id=message.chat.id)
-            queue.put((1, enable_codes_task))
+            queue.queue.put((1, enable_codes_task))
 
     @bot.message_handler(commands=['codes_off'])
     def disable_codes(message):
@@ -503,7 +503,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             disable_codes_task = Task(message.chat.id, 'codes_off', session_id=message.chat.id)
-            queue.put((0, disable_codes_task))
+            queue.queue.put((0, disable_codes_task))
 
     @bot.message_handler(commands=['add_tag'])
     def add_tag(message):
@@ -538,7 +538,7 @@ def run_app():
                 else:
                     pass
             send_live_location_task = Task(message.chat.id, 'live_location', session_id=main_chat_id, coords=coords, duration=duration)
-            queue.put((99, send_live_location_task))
+            queue.queue.put((99, send_live_location_task))
 
     @bot.message_handler(commands=['stop_ll'])
     def stop_live_location(message):
@@ -549,7 +549,7 @@ def run_app():
             point_number = re.findall(r'\s+(\d{1,2})', str(message.text.encode('utf-8')))
             point = point_number[0] if point_number else None
             stop_live_location_task = Task(message.chat.id, 'stop_live_location', session_id=main_chat_id, point=point)
-            queue.put((99, stop_live_location_task))
+            queue.queue.put((99, stop_live_location_task))
 
     @bot.message_handler(commands=['edit_ll'])
     def edit_live_location(message):
@@ -564,7 +564,7 @@ def run_app():
             point_number = re.findall(r'\s(\d{1,2})\s', str(message.text.encode('utf-8')))
             point = point_number[0] if point_number else None
             edit_live_location_task = Task(message.chat.id, 'edit_live_location', session_id=main_chat_id, point=point, coords=coords)
-            queue.put((99, edit_live_location_task))
+            queue.queue.put((99, edit_live_location_task))
 
     @bot.message_handler(commands=['clean_ll'])
     def clean_ll(message):
@@ -573,7 +573,7 @@ def run_app():
 
             main_chat_id = message.chat.id if message.chat.id in main_chat_ids else DB.get_main_chat_id_via_add(message.chat.id)
             clean_live_location_task = Task(message.chat.id, 'clean_ll', session_id=main_chat_id)
-            queue.put((99, clean_live_location_task))
+            queue.queue.put((99, clean_live_location_task))
 
     @bot.message_handler(commands=['addll'])
     def add_points_live_location(message):
@@ -601,7 +601,7 @@ def run_app():
                 else:
                     pass
             add_points_ll_task = Task(message.chat.id, 'add_points_ll', session_id=main_chat_id, points_dict=points_dict, duration=duration)
-            queue.put((99, add_points_ll_task))
+            queue.queue.put((99, add_points_ll_task))
 
     @bot.message_handler(commands=['get_codes_links'])
     def get_codes_links(message):
@@ -610,7 +610,7 @@ def run_app():
                 and Validations.check_from_main_chat(message.chat.id, bot, main_chat_ids, message.message_id):
 
             get_codes_links_task = Task(message.chat.id, 'get_codes_links', session_id=message.chat.id, message_id=message.message_id)
-            queue.put((99, get_codes_links_task))
+            queue.queue.put((99, get_codes_links_task))
 
     @bot.message_handler(commands=['kml'])
     def get_map_file(message):
@@ -635,7 +635,7 @@ def run_app():
                     'description': re.findall(r'\d\d\.\d{4,7}\s*-\s*(.+)', point, re.U)[0] if re.findall(r'\d\d\.\d{4,7}\s*-\s*(.+)', point, re.U) else None
                 })
             get_map_file_task = Task(message.chat.id, 'get_map_file', session_id=main_chat_id, points=points, message_id=message.message_id)
-            queue.put((99, get_map_file_task))
+            queue.queue.put((99, get_map_file_task))
 
     @bot.message_handler(commands=['instruction'])
     def send_instruction(message):
@@ -650,7 +650,7 @@ def run_app():
             code = re.findall(r'[!\./]\s*(.+)', str(message.text.lower().encode('utf-8')))[0]
             send_code_main_task = Task(message.chat.id, 'send_code_main', session_id=main_chat_id, code=code,
                                        message_id=message.message_id)
-            queue.put((1, send_code_main_task))
+            queue.queue.put((1, send_code_main_task))
 
     @bot.message_handler(regexp='^\?\s*(.+)')
     def bonus_code_processor(message):
@@ -661,7 +661,7 @@ def run_app():
             code = re.findall(r'\?\s*(.+)', str(message.text.lower().encode('utf-8')))[0]
             send_code_bonus_task = Task(message.chat.id, 'send_code_bonus', session_id=main_chat_id, code=code,
                                         message_id=message.message_id)
-            queue.put((1, send_code_bonus_task))
+            queue.queue.put((1, send_code_bonus_task))
 
     @bot.message_handler(regexp='\d\d\.\d{4,7},\s{0,3}\d\d\.\d{4,7}|'
                                 '\d\d\.\d{4,7}\s{0,3}\d\d\.\d{4,7}|'
@@ -673,7 +673,7 @@ def run_app():
             coords = find_coords(message.text)
             if coords:
                 send_coords_task = Task(message.chat.id, 'send_coords', coords=coords, message_id=message.message_id)
-                queue.put((99, send_coords_task))
+                queue.queue.put((99, send_coords_task))
 
     @app.route("/")
     def index():
@@ -787,3 +787,7 @@ def run_app():
 
 
 app = run_app()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=True)
